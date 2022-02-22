712d3e28f991c9df26b28225727a92375bf70375
Vikram S. Negi <viknegi0@gmail.com>
2022-02-22 19:43:04 +0530

    no changes

--- server/apis/createFile.js
+++ server/apis/createFile.js
@@ -6,11 +6,17 @@ const createFile = (data) => {
 
   const fileName = `${data.personalInfo[0].firstName.toLowerCase()}_${data.personalInfo[0].lastName.toLowerCase()}_data.json`;
 
-  fs.writeFile(`${__dirname}/userData/${fileName}`, JSON.stringify(data), { flag: "w" }, (err) => {
-    if (err) {
-      console.error(err, "createFile failed!");
-    }
-  });
+  // fs.writeFile(`${__dirname}/userData/${fileName}`, JSON.stringify(data), { flag: "w" }, (err) => {
+  //   if (err) {
+  //     console.error(err, "createFile failed!");
+  //   }
+  // });
+
+  try {
+    fs.writeFile(`${__dirname}/userData/${fileName}`, JSON.stringify(data), { flag: "w" });
+  } catch (err) {
+    console.error(err, "createFile failed!");
+  }
   
 }
 
