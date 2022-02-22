355059534f042f4a8801a6a6e3cad0fa9c8b8248
Vikram S. Negi <viknegi0@gmail.com>
2022-02-22 21:21:32 +0530

    moving things around

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { createFile } = require("./apis/createFile.js");
 
 const app = express();
 
-app.use("/", express.static(__dirname + "/../build"));
+app.use("/", express.static(__dirname + "/client/build"));
 
 const HOST = "localhost";
 const PORT = process.env.PORT || 8080;
@@ -24,9 +24,9 @@ app.listen(PORT, HOST, () => console.log(`listening on http://${HOST}:${PORT}`))
 app.post("/user-data", (req, res) => {
 
   const totalUserData = req.body;
-  // console.log(totalUserData);
+  console.log(totalUserData);
 
-  createFile(totalUserData);
+  // createFile(totalUserData);
   // console.log(message);
 
   res.json("Data saved in a JSON file 🎉");
