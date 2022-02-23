d16758243be02cc5fc3edba776b45090bace9e30
Vikram S. Negi <viknegi0@gmail.com>
2022-02-23 09:25:32 +0530

    home page complete

--- client/src/Home.js
+++ client/src/Home.js
@@ -3,10 +3,19 @@ import { Link } from "react-router-dom"
 
 export default function Home() {
   return (
-    <div>
-      <h1>Welcome to the Enivesh Form Webpage 👋</h1>
-      <p>This form was built with the purpose of using the data to determine our customer's financial postion.</p>
-      <Link to="/form" className="fill-form-btn">Fill up the form</Link>
+    <div className="main-home">
+      <h1>Welcome to the Enivesh Goal Organizer Webpage 👋</h1>
+      <p>This site was built with the purpose of using the form data to determine our customers' financial postion.</p>
+      <h2>Intructions:</h2>
+      <ul>
+        <li>Make sure to fill-in all the details carefully</li>
+        <li>After filling a certain form click the <code>Save</code> button</li>
+        <li>Clicking on the <code>Save</code> sends you a notifications</li>
+        <li>You can add additional information after clicking on the save button</li>
+        <li>You may see a tick next to the <code>Save</code> button, this shows that you have filled this form before</li>
+        <li>Finally, click on the <code className="submitFormBtn">Submit Form</code> button to complete the process</li>
+      </ul>
+      <Link to="/form" className="fill-form-btn">Fill the form</Link>
     </div>
   );
 }
