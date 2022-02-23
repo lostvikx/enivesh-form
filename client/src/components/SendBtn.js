d2e24a97340caf489ea6410b7c85844f9adfadb9
Vikram S. Negi <viknegi0@gmail.com>
2022-02-23 10:23:07 +0530

    able to send email

--- client/src/components/SendBtn.js
+++ client/src/components/SendBtn.js
@@ -1,5 +1,8 @@
 import React from "react";
 import { totalUserData, alertUser } from "./Form";
+import emailjs from 'emailjs-com';
+
+let fullName = null;
 
 const handleClick = async (evt) => {
 
@@ -16,33 +19,52 @@ const handleClick = async (evt) => {
 
   }
 
+  // if (formPass) {
+  //   try {
+  //     const res = await fetch("/user-data", {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json"
+  //       },
+  //       body: JSON.stringify(totalUserData)
+  //     });
+    
+  //     const data = await res.json();
+    
+  //     console.log(data);
+  //     window.location.href = "/thanks";
+  //   } catch (err) {
+  //     console.error(err, "couldn't submit the data!");
+  //   }
+  // } else {
+  //   alertUser("Please fill the entire form! 🤖", "red");
+  // }
+
   if (formPass) {
-    try {
-      const res = await fetch("/user-data", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify(totalUserData)
-      });
-    
-      const data = await res.json();
-    
-      console.log(data);
-      window.location.href = "/thanks";
-    } catch (err) {
-      console.error(err, "couldn't submit the data!");
-    }
-  } else {
-    alertUser("Please fill the entire form! 🤖", "red");
+
+    fullName = `${totalUserData.personalInfo[0].firstName[0].toUpperCase() + totalUserData.personalInfo[0].firstName.slice(1,)} ${totalUserData.personalInfo[0].lastName[0].toUpperCase() + totalUserData.personalInfo[0].lastName.slice(1,)}`;
+    // console.log(fullName);
+
+    document.getElementById("totalUserData").value = JSON.stringify(totalUserData);
+    document.getElementById("fullName").value = fullName;
+
+    //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior)
+    emailjs.sendForm('service_wv9fgeb', 'template_j5uu2sf', evt.target, 'user_pvGioTjQ0ItUmSUvav5qf')
+      .then((result) => window.location.href = "/thanks", (error) => console.error(error));
   }
 
 }
 
 export default function SendBtn() {
   return (
-    <button onClick={handleClick} className="save-data">
-      Submit Form
-    </button>
+    // <button onClick={handleClick} className="save-data">
+    //   Submit Form
+    // </button>
+
+    <form onSubmit={handleClick}>
+      <textarea id="totalUserData" name="totalUserData" hidden />
+      <input type="text" name="fullName" hidden id="fullName" />
+      <button type="submit" className="save-data">Send Mail</button>
+    </form>
   );
 }
